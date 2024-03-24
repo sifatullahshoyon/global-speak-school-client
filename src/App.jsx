8957f107adbf9d144e0f32e6f71fdbba529d3a3b
freelancersifat627e <freelancersifat627@gmail.comm>
2024-03-24 07:31:14 +0600

    initial setup done for ready project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-red-600 underline font-roboto'>GlobalSpeak School</h1>
+    </div>
+  );
+};
+
+export default App;

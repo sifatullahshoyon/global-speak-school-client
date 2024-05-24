b69cc4c631994e850fa92c69d87bc0c3cf814e7c
freelancersifat627e <freelancersifat627@gmail.comm>
2024-05-24 16:30:20 +0600

    update some code

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -64,7 +64,7 @@ const router = createBrowserRouter([
       },
       {
         path: "payment",
-        element: <Payment />,
+        element: <PrivateRoute><Payment /></PrivateRoute>,
       },
       // admin route
       {
